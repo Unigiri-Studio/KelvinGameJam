ea8f59055976d48791109a8e2b9c8ed8e34014b9
Acul4321 <lucarogers666@gmail.com>
2024-11-04 19:33:13 +0000

    player states added

--- kelvin's-katch/script/player.gd
+++ kelvin's-katch/script/player.gd
@@ -6,10 +6,20 @@ var acceleration = 100.0 # How fast the boat accelerates
 var maxSpeed = 400.0 # Maximum speed the boat can reach
 var drag = 0.95 # Drag coefficient (should be between 0 and 1, where 1 is no drag)
 
+#player states
+enum SAILSTATE {IDLE, SAILING,FISHING}
+enum FISHSTATE {AIMING,CASTED,WAITING,CATCHED}
+var state = SAILSTATE.IDLE #base state
+
 func _physics_process(delta):
-	#get sailing Direction
-	var direction = Input.get_vector("sailLeft", "sailRight", "sailForward", "sailBackwards").normalized()
-	direction = Vector3(direction.x,0,direction.y) #convert vector2 into vector3
+	var direction = Vector3.ZERO
+	if(state == SAILSTATE.SAILING or state == SAILSTATE.IDLE): # player sailing
+		#get sailing Direction
+		direction = Input.get_vector("sailLeft", "sailRight", "sailForward", "sailBackwards").normalized()
+		direction = Vector3(direction.x,0,direction.y) #convert vector2 into vector3
+		state = SAILSTATE.SAILING
+	elif(velocity == Vector3.ZERO): #player stopped
+		state = SAILSTATE.IDLE
 	
 	#apply acceleration
 	var accel = direction * acceleration * delta
