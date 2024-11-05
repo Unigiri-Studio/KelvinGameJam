41c3b145653122b0ea1647616662c9ce2910c26e
Acul4321 <lucarogers666@gmail.com>
2024-11-05 11:57:41 +0000

    added accuracy to lure

--- kelvin's-katch/script/player/player.gd
+++ kelvin's-katch/script/player/player.gd
@@ -13,10 +13,12 @@ var state: STATE = STATE.IDLE #base state
 #fishing variables
 var fishPlane:Plane #used for fishing and for fish logic
 @export var camera : Camera3D
-#lure location variables
-var lureTween: Tween = null
+
+#lure accuracy variables
+var ringTween: Tween = null
 var minScale : Vector3 = Vector3(0.1,0.1,0.1)
 var shrinkDuration = 5
+@onready var lure = %fishingLure
 
 func _ready():
 	fishPlane = Plane(Vector3.UP, Vector3.ZERO)
@@ -67,16 +69,15 @@ func _physics_process(delta):
 				camera.project_ray_normal(get_viewport().get_mouse_position()))
 			
 			#update lure location
-			%lure.global_position = worldPosition
+			%fishingRing.global_position = worldPosition
 			
 			if Input.is_action_just_pressed("castRod"):
 				changeState(STATE.CASTING)
 		
 		STATE.CASTING:
 			if !Input.is_action_pressed("castRod"): #while focusing lure location
-				if lureTween:
-					lureTween.stop()
-				#TODO Cast inside raduis circle logic here
+				if ringTween:
+					ringTween.stop()
 				changeState(STATE.WAITING)
 			
 		STATE.WAITING:
@@ -89,20 +90,25 @@ func changeState(newState: STATE) -> void:
 	state = newState
 	match state:
 		STATE.IDLE:
-			%lure.visible = false
+			%fishingRing.visible = false
 			print("Idle")
 		STATE.SAILING:
-			%lure.visible = false
+			%fishingRing.visible = false
 			print("Sailing")
 		STATE.AIMING:
-			%lure.scale = Vector3i(1,1,1)
-			%lure.visible = true
+			%accuracyRing.scale = Vector3i(1,1,1)
+			%fishingRing.visible = true
+			lure.visible = false
 			print("Aiming")
 		STATE.CASTING:
-			lureTween = get_tree().create_tween()
-			lureTween.tween_property(%lure, "scale", minScale, 1)
+			ringTween = get_tree().create_tween()
+			ringTween.tween_property(%accuracyRing, "scale", minScale, shrinkDuration)
 			print("Casting")
 		STATE.WAITING:
+			var randomLurePos : Vector3 = getRandomPosInsideMesh(%accuracyRing)
+			lure.global_position = randomLurePos
+			lure.visible = true
+			%fishingRing.visible = false
 			print("Waiting")
 
 func clampVector3(vec3: Vector3) -> Vector3:
@@ -134,3 +140,21 @@ func faceDir(vec3 : Vector3) -> void:
 		play_animation("idleNW")
 	elif(vec3 == Vector3(1,0,1)):
 		play_animation("idleSE")
+
+func getRandomPosInsideMesh(mesh : MeshInstance3D) -> Vector3:
+	#using cartisean coodinates will get random point inside cylindarMesh
+	var aabb = mesh.get_aabb() # get bounding box of mesh
+	
+	var curScale = mesh.scale.x #the scale can vary, but uniform so 1point is only needed
+	var radius = aabb.size.x / 2 * curScale
+	#var height = aabb.size.y * curScale
+	
+	#polar to cartisan coordinates convertion
+	var angle = randf() * PI * 2 #random angle 0-2PI
+	var distance = sqrt(randf()) * radius #random distance within radius
+	
+	var cartX = cos(angle) * distance
+	var cartY = 0#randf_range(0,height)
+	var cartZ = sin(angle) * distance
+	
+	return mesh.global_position + Vector3(cartX,cartY,cartZ)
