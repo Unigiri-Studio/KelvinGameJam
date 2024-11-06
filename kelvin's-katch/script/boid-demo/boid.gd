6e7a2deb2414ea32d294103e688544eeaf3b267b
Luca Rogers <124876210+Acul4321@users.noreply.github.com>
2024-11-06 17:11:18 +0000

    changed order and cleaned up file tree

--- kelvin's-katch/script/boid-demo/boid.gd
+++ kelvin's-katch/script/boid-demo/boid.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var fish_scene = load("res://scene/fish.tscn")
+var fish_scene = load("res://scene/boid-demo/fish.tscn")
 const FISH_COUNT = 100  # number of fish
 const X_RANGE = Vector2(-90, 90)  # x-axis
 const Z_RANGE = Vector2(-90, 90)  # z-axis
