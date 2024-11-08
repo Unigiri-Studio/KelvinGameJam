0ab094244ca9f796a1e811427f6744973cf2cc44
Luca Rogers <124876210+Acul4321@users.noreply.github.com>
2024-11-08 22:13:51 +0000

    added fish shadow

--- kelvin's-katch/script/fish/fish_class.gd
+++ kelvin's-katch/script/fish/fish_class.gd
@@ -5,6 +5,7 @@ class_name fish_class
 # Constants for movement and behavior
 @export var swiming_type: int #1: linear, 2: stuttered
 @export var fish_sprite: CompressedTexture2D
+@export var swimming_sprite:CompressedTexture2D
 @export var speed: float
 @export var depth: float
 @export var direction_change_interval: float
